ff2cecf6540636724469f42479e29c8cbd493b2a
Dronjak <berkaydin8@gmail.com>
2017-11-29 00:19:45 +0300

    Deneme commiti

---
+++ Assets/Scripts/Deneme Scripti.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DenemeScripti : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// deneme hocam
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
